4e2f81d1b184712fa6befe2383db04c4e1f2320d
unknown <jaqms90@gmail.com>
2016-11-10 09:57:52 -0500

    Clean up render example

--- examples/render.js
+++ examples/render.js
@@ -9,9 +9,7 @@ var HEIGHT = 480;
 var stage = new PIXI.Stage(0x000000);
 var renderer = new PIXI.CanvasRenderer(WIDTH, HEIGHT);
 var TILESET = null;
-var cacheCanvas = null;
-var textureCache = null;
-var tilePalette = null;
+var world = null;
 
 function testTexturePacking() {
     for (var i = 0; i < 100; i++) {
@@ -36,49 +34,67 @@ function run() {
 function onResourcesLoaded() {
     document.body.appendChild(renderer.view);
 
-    cacheCanvas = document.createElement('canvas');
+    var cacheCanvas = document.createElement('canvas');
     cacheCanvas.width = 640;
     cacheCanvas.height = 640;
 
-    textureCache = new Graphics.TextureCache(cacheCanvas, TILESET);
+    var textureCache = new Graphics.TextureCache(cacheCanvas, TILESET);
 
-    tilePalette = new Graphics.TilePalette();
+    var tilePalette = new Graphics.TilePalette();
     tilePalette.setEntry(0, new Graphics.Tile({fg: 0x000000, bg: 0x000000, char: 0}));
     tilePalette.setEntry(1, new Graphics.Tile({fg: 0xFF0000, bg: 0x00FF00, char: 100}));
     tilePalette.setEntry(2, new Graphics.Tile({fg: 0xFF0000, bg: 0x000000, char: 219}));
     tilePalette.setEntry(3, new Graphics.Tile({fg: 0x0000FF, bg: 0x000000, char: 219}));
     tilePalette.setEntry(4, new Graphics.Tile({fg: 0xFFFFFF, bg: 0x000000, char: 7}));
 
-    spatial = new Physics.Spatial(new Physics.GridHash(32));
+    var spatial = new Physics.Spatial(new Physics.GridHash(32));
 
-    sprites = {
+        entities = {
         player: {
-            stage: stage,
-            cache: textureCache,
-            tiles: tilePalette.convertToTiles(
-                [3, 3,
-                 3, 3]),
-            width: 2, height: 2,
-            x: 320, y: 240
+            sprite: {
+                stage: stage,
+                cache: textureCache,
+                tiles: tilePalette.convertToTiles(
+                    [3, 3,
+                     3, 3]),
+                width: 2, height: 2,
+                x: 320, y: 240
+            },
+            body: {
+                bounds: new PIXI.Rectangle(0, 0, 16, 16),
+                spatial: spatial
+            }
         },
         enemy: {
-            stage: stage,
-            cache: textureCache,
-            tiles: tilePalette.convertToTiles(
-                [2, 2, 2,
-                 2, 2, 2,
-                 2, 2, 2]),
-            width: 3, height: 3,
-            x: 0, y: 0
+            sprite: {
+                stage: stage,
+                cache: textureCache,
+                tiles: tilePalette.convertToTiles(
+                    [2, 2, 2,
+                     2, 2, 2,
+                     2, 2, 2]),
+                width: 3, height: 3,
+                x: 0, y: 0
+            },
+            body: {
+                bounds: new PIXI.Rectangle(0, 0, 24, 24),
+                spatial: spatial
+            }
         },
         bullet: {
-            stage: stage,
-            cache: textureCache,
-            tiles: tilePalette.convertToTiles([4]),
-            width: 1, height: 1,
-            x: 0, y: 0
+            sprite: {
+                stage: stage,
+                cache: textureCache,
+                tiles: tilePalette.convertToTiles([4]),
+                width: 1, height: 1,
+                x: 0, y: 0
+            },
+            body: {
+                bounds: new PIXI.Rectangle(0, 0, 8, 8),
+                spatial: spatial
+            }
         }
-    }
+    };
 
     tilemaps = {
         board: {
@@ -93,30 +109,15 @@ function onResourcesLoaded() {
             width: 5, height: 5,
             x: 0, y: 0
         }
-    }
+    };
 
-    bodies = {
-        player: {
-            bounds: new PIXI.Rectangle(0, 0, 16, 16),
-            spatial: spatial
-        },
-        enemy: {
-            bounds: new PIXI.Rectangle(0, 0, 24, 24),
-            spatial: spatial
-        },
-        bullet: {
-            bounds: new PIXI.Rectangle(0, 0, 8, 8),
-            spatial: spatial
-        }
-    }
-
-    RenderParser = new ZZT.Parser();
+    var RenderParser = new ZZT.Parser();
     RenderParser.registerModule('default', ZZT.DefaultCommandSet);
     RenderParser.registerModule('pixi', Graphics.PIXICommandSet);
     RenderParser.registerModule('body', Physics.PhysicsCommandSet);
     RenderParser.registerModule('input', Input.InputCommandSet);
 
-    board = new ZZT.Board();
+    var board = new ZZT.Board();
 
     board.configure({
         autoStep: false,
@@ -124,12 +125,13 @@ function onResourcesLoaded() {
     });
 
     board.setup(function () {
-        object('Player', ['_x', '_y'], function () {
-            adopt('body', bodies.player)
-            adopt('pixi', sprites.player)
-            adopt('input')
-            body.move_to($('_x'), $('_y'))
-            jump('move')
+        object('Player', function () {
+            label('init', ['_x', '_y'])
+                adopt('body', entities.player.body)
+                adopt('pixi', entities.player.sprite)
+                adopt('input')
+                body.move_to($('_x'), $('_y'))
+                jump('move')
             end()
 
             label('move')
@@ -159,32 +161,37 @@ function onResourcesLoaded() {
             end()
         });
 
-        object('Enemy', ['_x', '_y'], function () {
-            adopt('body', bodies.enemy)
-            adopt('pixi', sprites.enemy)
-            body.move_to($('_x'), $('_y'))
-            pixi.alpha(0.5)
-            jump('move')
+        object('Enemy', function () {
+            label('init', ['_x', '_y'])
+                adopt('body', entities.enemy.body)
+                adopt('pixi', entities.enemy.sprite)
+                body.move_to($('_x'), $('_y'))
+                pixi.alpha(0.5)
+                jump('move')
             end()
 
             label('move')
                 body.move('/rnd')
+                body.move('/flow/flow/flow/flow')
                 jump('move')
             end()
 
             label('enemy_stop')
                 print("OUCH")
-            die()
+                die()
+            end()
         });
 
-        object('Bullet', ['_x', '_y', '_dir'], function () {
-            adopt('body', bodies.bullet)
-            adopt('pixi', sprites.bullet)
-            body.move_to($('_x'), $('_y'))
-            _if('$_dir === "e"')
-                body.move('/e')
-            _endif()
-            jump('loop')
+        object('Bullet', function () {
+            label('init', ['_x', '_y', '_dir'])
+                adopt('body', entities.bullet.body)
+                adopt('pixi', entities.bullet.sprite)
+                body.move_to($('_x'), $('_y'))
+                _if('$_dir === "e"')
+                    body.move('/e')
+                _endif()
+                jump('loop')
+            end()
 
             label('loop')
                 _if(body.blocked('flow'))
@@ -196,13 +203,17 @@ function onResourcesLoaded() {
             end()
 
             label('stop')
-            die()
+                die()
+            end()
         })
     });
     board.run(function () {
         adopt('pixi', tilemaps.board)
         loop(100)
-            spawn('Enemy', [expr('Math.floor(Math.random() * 640 / 8) * 8'), expr('Math.floor(Math.random() * 480 / 8) * 8')])
+            spawn('Enemy', [
+                expr('Math.floor(Math.random() * 640 / 8) * 8'),
+                expr('Math.floor(Math.random() * 480 / 8) * 8')
+            ])
         endloop()
         spawn('Player', [640 / 2, 480 / 2])
         end()
